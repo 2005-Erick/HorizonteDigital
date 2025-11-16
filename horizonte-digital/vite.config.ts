fd75972760d3d587f2e1016119ec31d01ab0241b
2005-Erick <erickmelo0202@gmail.com>
2025-11-15 22:29:16 -0300

    Add vite build and dist files

--- horizonte-digital/vite.config.ts
+++ horizonte-digital/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "horizonte-digital/",
+  base: "/horizonte-digital",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
